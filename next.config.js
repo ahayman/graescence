3e815e74c0790f466d8ec8846c1890d227d7016a
Aaron Hayman <aaron@flexilesoft.com>
2023-01-10 15:00:41 -0500

    reverted export config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  assetPrefix: isProd ? '/graescence/' : '',
+  // assetPrefix: isProd ? '/graescence/' : '',
   images: {
     unoptimized: true,
   },
