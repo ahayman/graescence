1114f746b4c0391f90dcb279c680cf562f74735a
Aaron Hayman <aaronhayman@Aarons-MacBook-Air.local>
2023-01-10 13:02:42 -0500

    prod next configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  assetPrefix: isProd ? '/graescence/' : '',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   experimental: {
     appDir: true,
