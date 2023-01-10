9fe8af1de80eab7a441891b340ac20885afb4229
Aaron Hayman <aaronhayman@Aarons-MacBook-Air.local>
2023-01-10 11:05:27 -0500

    Remove next config pages configuration (they're injected in the pipeline)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  assetPrefix: isProd ? '/graescence/' : '',
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
   experimental: {
     appDir: true,
