28991219a61aa99daf8e7a2e0144b2c13d22c148
Aaron Hayman <aaron@flexilesoft.com>
2023-09-30 08:04:57 -0400

    Update Build config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
