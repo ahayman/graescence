1805fe62d4ff1fbba34c5c33eae33e4b0f0e9883
Aaron Hayman <aaron@flexilesoft.com>
2025-07-14 12:09:45 -0400

    M  next.config.js D  vercel.json

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  outputFileTracingExcludes: {
+    '/app/**/*': ['/content/**/*'],
+  },
 }
 
 module.exports = nextConfig
