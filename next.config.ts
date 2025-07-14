4d7402cdc97a6e01b55cd14210706c9551f79c57
Aaron Hayman <aaron@flexilesoft.com>
2025-07-14 15:31:49 -0400

    M  next.config.ts M  package-lock.json M  package.json

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 import { NextConfig } from 'next'
+
 const path = require('path')
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -19,6 +21,37 @@ const nextConfig: NextConfig = {
       './markdown-content/Lore',
       './markdown-content/Pages',
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: 'markdown-content/Blog/', // Source folder for your content files
+              to: 'markdown-content/Blog/', // Destination folder within .next/server
+            },
+            {
+              from: 'markdown-content/Chapters/', // Source folder for your content files
+              to: 'markdown-content/Chapters/', // Destination folder within .next/server
+            },
+            {
+              from: 'markdown-content/History/', // Source folder for your content files
+              to: 'markdown-content/History/', // Destination folder within .next/server
+            },
+            {
+              from: 'markdown-content/Lore/', // Source folder for your content files
+              to: 'markdown-content/Lore/', // Destination folder within .next/server
+            },
+            {
+              from: 'markdown-content/Pages/', // Source folder for your content files
+              to: 'markdown-content/Pages/', // Destination folder within .next/server
+            },
+          ],
+        }),
+      )
+    }
+    return config
   },
 }
 
