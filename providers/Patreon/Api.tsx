92b4be47b5b96c8609cdf284357c01c9a956dcb9
Aaron Hayman <aaron@flexilesoft.com>
2025-07-14 19:31:59 -0400

    M  providers/Patreon/Api.tsx

--- providers/Patreon/Api.tsx
+++ providers/Patreon/Api.tsx
@@ -1,6 +1,6 @@
 export const clientID = 'o1E-8WNlsQW2HCPxXlsNtQl_IkHwoX3BpXc5gvq_e0L-0ihs0SQdhWSTlt0zge0e'
 export const clientSecret = 'qxjMD75ooFVMwW6TOPPuffPYbO3m_udYzL4hmPgZtEL3tUUkVhcRjgqP0xxGvgiN'
-export const redirectUrl = 'https://graescence.vercel.app/patreon/auth/redirect'
+export const redirectUrl = 'https://graescence.com/patreon/auth/redirect'
 const creatorAccessToken = '8OHit6xWIQQ6wFB-88S4Oehl3VM9bd6Hhzh9E9LVS6Q'
 const creatorRefreshToken = '312Ttm1tXFw49eqkDwAyFAaFBewO3Mn5C1xUohnuUHI'
 
