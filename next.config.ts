5d9322d1e2c234b81ed2214c0ee75ff271c0b3e0
Aaron Hayman <aaron@flexilesoft.com>
2025-07-14 15:42:25 -0400

    M  next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
     '/**': ['./markdown-content/Other'],
   },
   outputFileTracingIncludes: {
-    '/**': [
+    '/app/**': [
       './markdown-content/Blog',
       './markdown-content/Chapters',
       './markdown-content/History',
