9a1dcef4f2ec7fef406055c410b28c6ce82eaae4
Aaron Hayman <aaron@flexilesoft.com>
2025-07-17 16:10:36 -0400

    M  providers/Patreon/Api.tsx

--- providers/Patreon/Api.tsx
+++ providers/Patreon/Api.tsx
@@ -16,11 +16,11 @@ export const getPatreonLoginUrl = (path: string, installId: string) => {
     redirectUrl: encodeURIComponent(path),
     installId: isPWA() ? installId : undefined, // InstallIds are only needed for PWAs
   }
-  return `https://www.patreon.com/oauth2/authorize? 
-response_type=code
-&client_id=${process.env.NEXT_PUBLIC_PATREON_CLIENT_ID}
-&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_PATREON_REDIRECT_URL ?? '')}
-&state=${JSON.stringify(state)}`
+  return `https://www.patreon.com/oauth2/authorize?response_type=code&client_id=${
+    process.env.NEXT_PUBLIC_PATREON_CLIENT_ID
+  }&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_PATREON_REDIRECT_URL ?? '')}&state=${JSON.stringify(
+    state,
+  )}`
 }
 
 export const fetchAuthSignIn = async (code: string, installId?: string): Promise<AuthWithExpiration> => {
