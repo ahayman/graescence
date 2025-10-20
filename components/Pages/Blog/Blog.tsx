1ae47f39c493beec179df59ae557d736c45be1b2
Aaron Hayman <aaron@flexilesoft.com>
2025-10-20 10:37:22 -0400

    M  components/Pages/Blog/Blog.tsx

--- components/Pages/Blog/Blog.tsx
+++ components/Pages/Blog/Blog.tsx
@@ -13,7 +13,7 @@ const Blog = ({ blog }: Props) => (
   <Column className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
     <Header type="Primary" title="Updates" />
     {blog.map(item => (
-      <ExcerptItem tier="free" key={item.slug} {...item} />
+      <ExcerptItem tier="free" key={item.slug} {...item} date={item.publicDate} />
     ))}
   </Column>
 )
